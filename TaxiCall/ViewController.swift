0230c7ae30757d3857ca0aa372b9a7a1af08e217
雨夜 <kiras0518@gmail.com>
2018-10-12 23:08:43 +0800

    優化畫面 & 新增 icon

--- TaxiCall/ViewController.swift
+++ TaxiCall/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userModelSwitch: UISwitch!
+    @IBOutlet weak var loginView: UIView!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var buttonOutlet: UIButton!
+   
+    
+    @IBAction func signInButton(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setColor(customView: loginView, radius: 10)
+        setColor(customView: buttonOutlet, radius: 10)
     }
 
-
+    func setColor(customView : UIView, radius : CGFloat) {
+        customView.layer.cornerRadius = radius
+        customView.clipsToBounds = false
+        
+        setTextField(customTextField: email, iconName: "user")
+        
+        setTextField(customTextField: password, iconName: "pass")
+    }
+    
+    func setTextField(customTextField : UITextField, iconName: String) {
+        
+        customTextField.leftViewMode = UITextField.ViewMode.always
+        
+        var iconView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        
+        iconView.contentMode = UIView.ContentMode.scaleAspectFit
+        iconView.image = UIImage(named: iconName)
+        
+        customTextField.leftView = iconView
+        
+        customTextField.borderStyle = UITextField.BorderStyle.line
+        customTextField.layer.borderWidth = 1
+        customTextField.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+    }
+    
 }
 
