2849e09864f601b428fd3e4c1c4bfda7eea66c56
雨夜 <kiras0518@gmail.com>
2018-10-19 17:49:57 +0800

    更新接單乘客介面

--- TaxiCall/RiderViewController.swift
+++ TaxiCall/RiderViewController.swift
@@ -18,33 +18,40 @@ class RiderViewController: UIViewController, MKMapViewDelegate, CLLocationManage
     var HasBeenCalled = false
     var userLocation = CLLocationCoordinate2D()
     let reference : DatabaseReference = Database.database().reference()
-    
+    var driverLocation = CLLocationCoordinate2D()
+    var driverOrder = false
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var buttonCall: UIButton!
     
     @IBAction func callTaxi(_ sender: UIButton) {
         
-        if let email = Auth.auth().currentUser?.email {
-        if HasBeenCalled {
+        if !driverOrder {
             
+            if let email = Auth.auth().currentUser?.email {
+                
+                if HasBeenCalled {
+                    
             reference.child("RiderRequests").queryOrdered(byChild: "email").queryEqual(toValue: email).observe(DataEventType.childAdded) { (dataSnapShot) in
-                dataSnapShot.ref.removeValue()
-                self.reference.child("RiderRequests").removeAllObservers()
+                        dataSnapShot.ref.removeValue()
+                        self.reference.child("RiderRequests").removeAllObservers()
+                    }
+                    
+                    cancelTaixMode()
+                    
+                } else {
+                    
+                    let riderRequestDic : [String : Any] = ["email": email, "latitude": userLocation.latitude, "longitude": userLocation.longitude]
+                    
+                    reference.child("RiderRequests").childByAutoId().setValue(riderRequestDic)
+                    
+                    callTaixMode()
+                    
+                }
             }
             
-            cancelTaixMode()
-           
-        } else {
-            
-            let riderRequestDic : [String : Any] = ["email": email, "latitude": userLocation.latitude, "longitude": userLocation.longitude]
-            
-            reference.child("RiderRequests").childByAutoId().setValue(riderRequestDic)
-            
-            callTaixMode()
-            
         }
-      }
+        
     }
 
     @IBAction func logOut(_ sender: UIBarButtonItem) {
@@ -76,12 +83,74 @@ class RiderViewController: UIViewController, MKMapViewDelegate, CLLocationManage
         }
         locationManger.startUpdatingLocation()
         
-        print(locationManger)
-        
+        //print(locationManger)
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (Timer) in
+            self.updateLocation()
+        }
         
     }
     
     
+    func updateLocation() {
+     
+        if let email = Auth.auth().currentUser?.email {
+            
+            reference.child("RiderRequests").queryOrdered(byChild: "email").queryEqual(toValue: email).observe(DataEventType.childAdded) { (dataSnapShot) in
+                
+                self.callTaixMode()
+                
+                if let driverRequestDic = dataSnapShot.value as? [String : Any] {
+                    
+                    if let driverLat = driverRequestDic["driverLat"] as? Double {
+                        
+                        if let driverLon = driverRequestDic["driverLon"] as? Double {
+                            
+                            self.driverLocation = CLLocationCoordinate2D(latitude: driverLat, longitude: driverLon)
+                            self.driverOrder = true
+                            self.reference.child("RiderRequests").removeAllObservers()
+                            
+                        }
+                    }
+                    
+                }
+                
+            }
+        }
+        
+    }
+    
+
+    func displayDriverAndRider() {
+        
+        let driverCLLocation = CLLocation(latitude: driverLocation.latitude, longitude: driverLocation.longitude)
+        let riderCLLocation = CLLocation(latitude: userLocation.latitude, longitude: userLocation.longitude)
+        
+        let distance = driverCLLocation.distance(from: riderCLLocation) / 1000
+        let roundedDistance = round(distance * 100) / 100
+        buttonCall.setTitle("司機距離剩餘位置 \(roundedDistance) km ", for: UIControl.State.normal)
+        
+        mapView.removeAnnotations(mapView.annotations)
+        
+        let riderAnnotation = MKPointAnnotation()
+        riderAnnotation.coordinate = userLocation
+        riderAnnotation.title = "你的位置"
+        mapView.addAnnotation(riderAnnotation)
+        
+        let driverAnnotation = MKPointAnnotation()
+        driverAnnotation.coordinate = driverLocation
+        driverAnnotation.title = "司機位置"
+        mapView.addAnnotation(driverAnnotation)
+        
+        let latDelta = abs(driverLocation.latitude - userLocation.latitude) * 2.5
+        let lonDelta = abs(driverLocation.longitude  - userLocation.longitude) * 2.5
+        
+        let region = MKCoordinateRegion(center: userLocation, span: MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta))
+        mapView.setRegion(region, animated: true)
+        
+    }
+
+
+
     func callTaixMode(){
         
         buttonCall.setTitle("取消", for: UIControl.State.normal)
@@ -104,17 +173,28 @@ class RiderViewController: UIViewController, MKMapViewDelegate, CLLocationManage
         
         if let cooridnate : CLLocationCoordinate2D = manager.location?.coordinate {
             
+            userLocation = cooridnate
             
-            let region = MKCoordinateRegion(center: cooridnate, span: MKCoordinateSpan(latitudeDelta: 0.018, longitudeDelta: 0.018))
+            if driverOrder {
+                
+                displayDriverAndRider()
+                
+            } else {
+                
+                let region = MKCoordinateRegion(center: cooridnate, span: MKCoordinateSpan(latitudeDelta: 0.018, longitudeDelta: 0.018))
+                
+                mapView.setRegion(region, animated: true)
+                
+                mapView.removeAnnotations(mapView.annotations)
+                
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = cooridnate
+                annotation.title = "我的位置"
+                mapView.addAnnotation(annotation)
+                
+            }
             
-            mapView.setRegion(region, animated: true)
             
-            mapView.removeAnnotations(mapView.annotations)
-            
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = cooridnate
-            annotation.title = "My Location"
-            mapView.addAnnotation(annotation)
             
             
             
